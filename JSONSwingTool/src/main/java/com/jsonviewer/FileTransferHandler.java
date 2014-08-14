07683ee8a04f5641965c4f17842ea651c256d57e
Jayram Rout <jayram.rout@7chapters.info>
2014-08-13 23:50:43 -0400

    Update

--- JSONSwingTool/src/main/java/com/jsonviewer/FileTransferHandler.java
+++ JSONSwingTool/src/main/java/com/jsonviewer/FileTransferHandler.java
@@ -3,8 +3,11 @@ package com.jsonviewer;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
@@ -12,7 +15,7 @@ import javax.swing.TransferHandler;
 
 /**
  * @author Jayram Rout
- *
+ * 
  */
 class FileTransferHandler extends TransferHandler {
 	private DataFlavor fileFlavor, stringFlavor;
@@ -32,14 +35,26 @@ class FileTransferHandler extends TransferHandler {
 		try {
 			if (hasFileFlavor(t.getTransferDataFlavors())) {
 				String str = null;
-				java.util.List files = (java.util.List) t.getTransferData(fileFlavor);
+				java.util.List files = (java.util.List) t
+						.getTransferData(fileFlavor);
 				for (int i = 0; i < files.size(); i++) {
 					File file = (File) files.get(i);
 					tpc.addTab(file.toString());
 				}
 			} else if (hasStringFlavor(t.getTransferDataFlavors())) {
-				JOptionPane.showMessageDialog(null, "Does not support Text Transfer", "Error", JOptionPane.ERROR_MESSAGE);
-				return false;
+				File tempFile = new File("temp.json");
+				Writer writer = null;
+				writer = new BufferedWriter(new FileWriter(tempFile));
+				writer.write(t.getTransferData(stringFlavor).toString());
+				writer.flush();
+				writer.close();
+				Helper.fileList.remove(tempFile.getAbsoluteFile().toString());
+				tpc.addTab(tempFile.getAbsoluteFile().toString());
+
+				// JOptionPane.showMessageDialog(null,
+				// "Does not support Text Transfer", "Error",
+				// JOptionPane.ERROR_MESSAGE);
+				// return false;
 			}
 		} catch (UnsupportedFlavorException ufe) {
 			System.out.println("importData: unsupported data flavor");
@@ -48,6 +63,7 @@ class FileTransferHandler extends TransferHandler {
 		}
 		return false;
 	}
+
 	public int getSourceActions(JComponent c) {
 		return COPY_OR_MOVE;
 	}
