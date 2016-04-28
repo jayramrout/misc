bf08209da11b3322cad2f08959c12ef93eaa8e93
jayramrout <jayram.rout@gmail.com>
2016-04-28 11:36:58 -0400

    Removed some modules

--- DataBaseTool/src/com/dbquery/constants/DBContants.java
+++
@@ -1,6 +0,0 @@
-package com.dbquery.constants;
-
-public class DBContants {
-	public static final String CONFIG_FILE_NAME = "/config.properties";
-	public static final String SCHEMA_FILE_NAME = "/NHSchemas.json";
-}
--- JSONSwingTool/src/main/java/com/jrout/jsonviewer/JSONConstants.java
+++
@@ -1,5 +0,0 @@
-package com.jrout.jsonviewer;
-
-public class JSONConstants {
-	public final static String DEFAULT = "Default";
-}
